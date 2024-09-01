7a451cf0171c8998a2b378ad70d0b53dfdfc920a
swayamchhaba <100463824+swayamchhaba@users.noreply.github.com>
2024-09-01 21:29:40 +0530

    Arrays file added

--- dsa_practice/src/Arrays/Arrays_1.java
+++ dsa_practice/src/Arrays/Arrays_1.java
@@ -1,8 +1,19 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class Arrays_1 {
 
     public static void main (String[] args) {
-        System.out.println("Hello World");
+
+//      initializing the array by using various methods
+        int[] arr = new int[5]; // default values are 0;
+        int[] arr_2 = {1, 2, 3, 4, 5};
+
+        Arrays.sort(arr_2);
+
+        for (int i : arr_2) {
+            System.out.println(i);
+        }
     }
 }
